2ccbb736ea550df2b8f01658f9162a5d93442a71
S. M. Crawford <crawford@saao.ac.za>
2016-07-15 16:50:06 +0200

    basic reduction for aries data

---
+++ aries_basic_reductions.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import numpy as np
+  
+from astropy.io import fits
+from astropy import units as u
+
+import ccdproc 
+from ccdproc import CCDData
+
+from ccdproc import ImageFileCollection
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Process Aries engineering observationss')
+parser.add_argument('infile', nargs='*', help='File or files to be processed')
+parser.add_argument('--b', dest='bias', help='Master bias file', default=None)
+parser.add_argument('-n', dest='cray', default=True, action='store_false', help='Do not cosmic ray clean')
+args = parser.parse_args()
+
+
+infiles = args.infile
+
+print infiles
+
+if args.bias:
+   mbias = CCDData.read(args.bias, unit = u.adu)
+else:
+   mbias = None
+
+for infile in infiles:
+    ccd = CCDData.read(infile, unit = u.adu)
+    ccd = ccdproc.ccd_process(ccd, oscan='[1117:1181, 1:330]', oscan_median=True, 
+                              trim='[17:1116,1:330]', master_bias=mbias)
+    if args.cray:
+       ccd = ccdproc.cosmicray_lacosmic(ccd, sigclip=4.5, sigfrac=0.3,
+                   objlim=5.0, gain=1.0, readnoise=6.5,
+                   satlevel=65536.0, pssl=0.0, niter=4,
+                   sepmed=True, cleantype='meanmask', fsmode='median',
+                   psfmodel='gauss', psffwhm=2.5, psfsize=7,
+                   psfk=None, psfbeta=4.765, verbose=False)
+    ccd.write('p'+os.path.basename(infile), clobber=True)
+
